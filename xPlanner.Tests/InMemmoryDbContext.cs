bbb9a788ce42c2f08fa8ea8f862fc7ec270d1fab
Егор Кравченко <egor2005krava@gmail.com>
2024-05-23 23:39:11 +0300

    writing first tests

---
+++ xPlanner.Tests/InMemmoryDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xPlanner.Data;
+
+namespace xPlanner.Tests
+{
+    internal class InMemmoryDbContext 
+    {
+        public static AppDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            var context = new AppDbContext(options);
+            context.Database.EnsureCreated();
+
+            context.SaveChanges();
+            return context;
+        }
+
+        public static void Destroy(AppDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+    }
+}
---
+++ xPlanner.Tests/UserServiceTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xPlanner.Auth;
+using xPlanner.Data.Repository;
+using xPlanner.Domain.Entities;
+using xPlanner.Services;
+
+namespace xPlanner.Tests
+{
+    public class UserServiceTest
+    {
+        private IUserService userService;
+
+        private int NotExistingUserId = 0;
+        private int ExistingUserId = 1;
+
+        public UserServiceTest()
+        {
+            var dbContext = InMemmoryDbContext.Create(); 
+            var repository = new UserRepository(dbContext);
+            userService = new UserService(repository, new PasswordHasher());
+        }
+
+        [Fact]
+        public async Task GetAllUsers_before_initialization_should_return_null()
+        {
+            // Arrange
+
+
+            // Act
+            
+            // Aseert
+
+        }
+
+        [Fact]
+        public async Task GetNotExistingUsers_Should_Reurns_Null()
+        {
+            // Arrange
+
+            // Act
+            MyProfileResponse result = await userService.GetUser(NotExistingUserId);
+
+            // Aseert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task GetExistingUser_Should_Returns_User()
+        {
+            // Arrange
+
+            // Act
+            MyProfileResponse? result = await userService.GetUser(ExistingUserId);
+
+            // Aseert
+            Assert.IsType<MyProfileResponse>(result);
+            // дизайн символика внешний вид персонала
+        }
+    }
+}
